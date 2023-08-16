d4b877f0e6a6b8e3dc30ad5a54e4d9a2a0bdcf2d
Aurélien <aurelien.strimelle@bstorm.be>
2023-08-16 11:05:20 +0200

    Change app icon with simple implementation

--- app/src/main/java/be/tftic/web2023/test_icon/MainActivity.kt
+++ app/src/main/java/be/tftic/web2023/test_icon/MainActivity.kt
@@ -1,13 +1,16 @@
 package be.tftic.web2023.test_icon
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import be.tftic.web2023.test_icon.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
 
-    private  lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +26,52 @@ class MainActivity : AppCompatActivity(){
     }
 
     private fun changeToIcon1() {
-        TODO("Not yet implemented")
+        // Use with activity-alias "MainActivityIcon1"
+        enableIcon("MainActivityIcon1")
     }
 
     private fun changeToIcon2() {
-        TODO("Not yet implemented")
+        // Use with activity-alias "MainActivityIcon1"
+        enableIcon("MainActivityIcon2")
     }
 
     private fun restoreInitialIcon() {
-        TODO("Not yet implemented")
+        // Use with activity "MainActivity"
+        enableIcon("MainActivity")
+    }
+
+    private fun enableIcon(iconName : String) {
+        // Disable all activity ()
+        disableAllIcon()
+
+        // Enable activity for use icon
+        packageManager.setComponentEnabledSetting(
+            ComponentName(
+                this@MainActivity,
+                "$packageName.$iconName"
+            ),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+    private fun disableAllIcon() {
+        val iconList = listOf<String>(
+            "MainActivityIcon1",
+            "MainActivityIcon2",
+            "MainActivity"
+        )
+
+        iconList
+            .forEach { icon ->
+                Log.d("disableOtherIcon", icon)
+            packageManager.setComponentEnabledSetting(
+                ComponentName(
+                    this@MainActivity,
+                    "$packageName.$icon"
+                ),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        }
     }
 }
